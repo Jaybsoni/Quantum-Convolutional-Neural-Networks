f9c292339c1692763c25359127da6a947eb16289
Jay Soni <jbsoni@uwaterloo.ca>
2021-04-16 12:34:35 -0400

    fixed up setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='qml',
       description='A library to build quantum convolutional neural networks',
       author='Jay Soni, Ivan Sharankov',
       packages=['qml'],
-      license='MIT', install_requires=['numpy', 'matplotlib']
+      license='MIT', install_requires=['numpy', 'matplotlib', 'scipy', 'qiskit']
       )
